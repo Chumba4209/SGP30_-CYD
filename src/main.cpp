c791e5e8d4b46dcb4ffd329cb479950cfd203477
Cynthia Kipruto <118298337+Chumba4209@users.noreply.github.com>
2026-03-16 12:43:51 +0300

    label edits

--- src/main.cpp
+++ src/main.cpp
@@ -17,22 +17,22 @@ TFT_eSPI tft = TFT_eSPI();
 Adafruit_SGP30 sgp;
 
 // Panels
-lv_obj_t *temp_panel;      // eCO2 gauge panel
-lv_obj_t *pressure_panel;  // TVOC gauge panel
+lv_obj_t *eco2_panel;      // eCO2 gauge panel
+lv_obj_t *tvoc_arc;  // TVOC gauge panel
 
 // Arcs
-lv_obj_t *temp_arc;        // eCO2 arc
-lv_obj_t *pressure_arc;    // TVOC arc
+lv_obj_t *eco2_arc;        // eCO2 arc
+lv_obj_t *tvoc_arc_arc;    // TVOC arc
 
 // Value labels
-lv_obj_t *temp_value_label;     // eCO2 value
-lv_obj_t *pressure_value_label; // TVOC value
+lv_obj_t *eco2_value_label;     // eCO2 value
+lv_obj_t *tvoc_arc_value_label; // TVOC value
 
 // Charts
 lv_obj_t *chart;
-lv_obj_t *pressure_chart;
-lv_chart_series_t *temp_series;
-lv_chart_series_t *pressure_series;
+lv_obj_t *tvoc_arc_chart;
+lv_chart_series_t *eco2_series;
+lv_chart_series_t *tvoc_arc_series;
 
 //  Display flush ─
 void my_disp_flush(lv_display_t *disp, const lv_area_t *area, uint8_t *px_map) {
@@ -91,10 +91,10 @@ void create_dashboard() {
     lv_obj_set_style_line_color(chart, lv_color_hex(0xEEEEEE), LV_PART_MAIN);
     lv_obj_set_style_size(chart, 0, 0, LV_PART_INDICATOR);
 
-    temp_series = lv_chart_add_series(chart, lv_color_hex(0x4CAF50),
+    eco2_series = lv_chart_add_series(chart, lv_color_hex(0x4CAF50),
                                       LV_CHART_AXIS_PRIMARY_Y);
     for(int i = 0; i < 20; i++) {
-        lv_chart_set_next_value(chart, temp_series, 400);
+        lv_chart_set_next_value(chart, eco2_series, 400);
     }
 
     // eCO2 scale labels
@@ -135,22 +135,22 @@ void create_dashboard() {
     lv_obj_align(tvoc_label, LV_ALIGN_TOP_LEFT, 2, 0);
 
     // TVOC chart
-    pressure_chart = lv_chart_create(tvoc_chart_panel);
-    lv_obj_set_size(pressure_chart, 140, 90);
-    lv_obj_align(pressure_chart, LV_ALIGN_BOTTOM_MID, 0, 0);
-    lv_chart_set_type(pressure_chart, LV_CHART_TYPE_LINE);
-    lv_chart_set_point_count(pressure_chart, 20);
-    lv_chart_set_range(pressure_chart, LV_CHART_AXIS_PRIMARY_Y, 0, 30);
-    lv_chart_set_div_line_count(pressure_chart, 4, 5);
-    lv_obj_set_style_bg_color(pressure_chart, lv_color_white(), 0);
-    lv_obj_set_style_border_width(pressure_chart, 0, 0);
-    lv_obj_set_style_line_color(pressure_chart, lv_color_hex(0xEEEEEE), LV_PART_MAIN);
-    lv_obj_set_style_size(pressure_chart, 0, 0, LV_PART_INDICATOR);
+    tvoc_arc_chart = lv_chart_create(tvoc_chart_panel);
+    lv_obj_set_size(tvoc_arc_chart, 140, 90);
+    lv_obj_align(tvoc_arc_chart, LV_ALIGN_BOTTOM_MID, 0, 0);
+    lv_chart_set_type(tvoc_arc_chart, LV_CHART_TYPE_LINE);
+    lv_chart_set_point_count(tvoc_arc_chart, 20);
+    lv_chart_set_range(tvoc_arc_chart, LV_CHART_AXIS_PRIMARY_Y, -5, 30);
+    lv_chart_set_div_line_count(tvoc_arc_chart, 4, 5);
+    lv_obj_set_style_bg_color(tvoc_arc_chart, lv_color_white(), 0);
+    lv_obj_set_style_border_width(tvoc_arc_chart, 0, 0);
+    lv_obj_set_style_line_color(tvoc_arc_chart, lv_color_hex(0xEEEEEE), LV_PART_MAIN);
+    lv_obj_set_style_size(tvoc_arc_chart, 0, 0, LV_PART_INDICATOR);
 
-    pressure_series = lv_chart_add_series(pressure_chart, lv_color_hex(0x2196F3),
+    tvoc_arc_series = lv_chart_add_series(tvoc_arc_chart, lv_color_hex(0x2196F3),
                                           LV_CHART_AXIS_PRIMARY_Y);
     for(int i = 0; i < 20; i++) {
-        lv_chart_set_next_value(pressure_chart, pressure_series, 15);
+        lv_chart_set_next_value(tvoc_arc_chart, tvoc_arc_series, 15);
     }
 
     // TVOC scale labels
@@ -173,102 +173,102 @@ void create_dashboard() {
     lv_obj_align(tvoc_min, LV_ALIGN_BOTTOM_RIGHT, -2, -8);
 
     //  eCO2 gauge panel (bottom left) 
-    temp_panel = lv_obj_create(scr);
-    lv_obj_set_size(temp_panel, 158, 100);
-    lv_obj_align(temp_panel, LV_ALIGN_BOTTOM_LEFT, 1, -1);
-    lv_obj_set_style_bg_color(temp_panel, lv_color_white(), 0);
-    lv_obj_set_style_border_width(temp_panel, 1, 0);
-    lv_obj_set_style_border_color(temp_panel, lv_color_hex(0xDDDDDD), 0);
-    lv_obj_set_style_radius(temp_panel, 6, 0);
-    lv_obj_set_style_pad_all(temp_panel, 4, 0);
+    eco2_panel = lv_obj_create(scr);
+    lv_obj_set_size(eco2_panel, 158, 100);
+    lv_obj_align(eco2_panel, LV_ALIGN_BOTTOM_LEFT, 1, -1);
+    lv_obj_set_style_bg_color(eco2_panel, lv_color_white(), 0);
+    lv_obj_set_style_border_width(eco2_panel, 1, 0);
+    lv_obj_set_style_border_color(eco2_panel, lv_color_hex(0xDDDDDD), 0);
+    lv_obj_set_style_radius(eco2_panel, 6, 0);
+    lv_obj_set_style_pad_all(eco2_panel, 4, 0);
 
-    lv_obj_t *eco2_title = lv_label_create(temp_panel);
+    lv_obj_t *eco2_title = lv_label_create(eco2_panel);
     lv_label_set_text(eco2_title, "eCO2");
     lv_obj_set_style_text_font(eco2_title, &lv_font_montserrat_14, 0);
     lv_obj_set_style_text_color(eco2_title, lv_color_hex(0x333333), 0);
     lv_obj_align(eco2_title, LV_ALIGN_TOP_LEFT, 4, 2);
 
     // eCO2 arc
-    temp_arc = lv_arc_create(temp_panel);
-    lv_obj_set_size(temp_arc, 70, 70);
-    lv_arc_set_rotation(temp_arc, 135);
-    lv_arc_set_bg_angles(temp_arc, 0, 270);
-    lv_arc_set_range(temp_arc, 395, 420);
-    lv_arc_set_value(temp_arc, 400);
-    lv_obj_remove_style(temp_arc, NULL, LV_PART_KNOB);
-    lv_obj_set_style_arc_color(temp_arc, lv_color_hex(0x4CAF50), LV_PART_INDICATOR);
-    lv_obj_set_style_arc_color(temp_arc, lv_color_hex(0xDDDDDD), LV_PART_MAIN);
-    lv_obj_set_style_arc_width(temp_arc, 8, LV_PART_INDICATOR);
-    lv_obj_set_style_arc_width(temp_arc, 8, LV_PART_MAIN);
-    lv_obj_align(temp_arc, LV_ALIGN_CENTER, 10, 8);
+    eco2_arc = lv_arc_create(eco2_panel);
+    lv_obj_set_size(eco2_arc, 90,90);
+    lv_arc_set_rotation(eco2_arc, 135);
+    lv_arc_set_bg_angles(eco2_arc, 0, 270);
+    lv_arc_set_range(eco2_arc, 395, 420);
+    lv_arc_set_value(eco2_arc, 400);
+    lv_obj_remove_style(eco2_arc, NULL, LV_PART_KNOB);
+    lv_obj_set_style_arc_color(eco2_arc, lv_color_hex(0x4CAF50), LV_PART_INDICATOR);
+    lv_obj_set_style_arc_color(eco2_arc, lv_color_hex(0xDDDDDD), LV_PART_MAIN);
+    lv_obj_set_style_arc_width(eco2_arc, 8, LV_PART_INDICATOR);
+    lv_obj_set_style_arc_width(eco2_arc, 8, LV_PART_MAIN);
+    lv_obj_align(eco2_arc, LV_ALIGN_CENTER, 10, 8);
 
-    temp_value_label = lv_label_create(temp_panel);
-    lv_label_set_text(temp_value_label, "400ppm");
-    lv_obj_set_style_text_font(temp_value_label, &lv_font_montserrat_14, 0);
-    lv_obj_set_style_text_color(temp_value_label, lv_color_hex(0x333333), 0);
-    lv_obj_align_to(temp_value_label, temp_arc, LV_ALIGN_CENTER, -12, 0);
+    eco2_value_label = lv_label_create(eco2_panel);
+    lv_label_set_text(eco2_value_label, "400ppm");
+    lv_obj_set_style_text_font(eco2_value_label, &lv_font_montserrat_14, 0);
+    lv_obj_set_style_text_color(eco2_value_label, lv_color_hex(0x333333), 0);
+    lv_obj_align_to(eco2_value_label, eco2_arc, LV_ALIGN_CENTER, -4, 0);
 
     // eCO2 scale labels
-    lv_obj_t *eco2_g_min = lv_label_create(temp_panel);
+    lv_obj_t *eco2_g_min = lv_label_create(eco2_panel);
     lv_label_set_text(eco2_g_min, "395");
     lv_obj_set_style_text_font(eco2_g_min, &lv_font_montserrat_14, 0);
     lv_obj_set_style_text_color(eco2_g_min, lv_color_hex(0x999999), 0);
-    lv_obj_align_to(eco2_g_min, temp_arc, LV_ALIGN_OUT_BOTTOM_LEFT, 0, -8);
+    lv_obj_align_to(eco2_g_min, eco2_arc, LV_ALIGN_OUT_BOTTOM_LEFT, 0, -8);
 
-    lv_obj_t *eco2_g_max = lv_label_create(temp_panel);
+    lv_obj_t *eco2_g_max = lv_label_create(eco2_panel);
     lv_label_set_text(eco2_g_max, "420");
     lv_obj_set_style_text_font(eco2_g_max, &lv_font_montserrat_14, 0);
     lv_obj_set_style_text_color(eco2_g_max, lv_color_hex(0x999999), 0);
-    lv_obj_align_to(eco2_g_max, temp_arc, LV_ALIGN_OUT_BOTTOM_RIGHT, 0, -8);
+    lv_obj_align_to(eco2_g_max, eco2_arc, LV_ALIGN_OUT_BOTTOM_RIGHT, 0, -8);
 
     //  TVOC gauge panel (bottom right) 
-    pressure_panel = lv_obj_create(scr);
-    lv_obj_set_size(pressure_panel, 158, 100);
-    lv_obj_align(pressure_panel, LV_ALIGN_BOTTOM_RIGHT, -1, -1);
-    lv_obj_set_style_bg_color(pressure_panel, lv_color_white(), 0);
-    lv_obj_set_style_border_width(pressure_panel, 1, 0);
-    lv_obj_set_style_border_color(pressure_panel, lv_color_hex(0xDDDDDD), 0);
-    lv_obj_set_style_radius(pressure_panel, 6, 0);
-    lv_obj_set_style_pad_all(pressure_panel, 4, 0);
+    tvoc_arc = lv_obj_create(scr);
+    lv_obj_set_size(tvoc_arc, 158, 100);
+    lv_obj_align(tvoc_arc, LV_ALIGN_BOTTOM_RIGHT, -1, -1);
+    lv_obj_set_style_bg_color(tvoc_arc, lv_color_white(), 0);
+    lv_obj_set_style_border_width(tvoc_arc, 1, 0);
+    lv_obj_set_style_border_color(tvoc_arc, lv_color_hex(0xDDDDDD), 0);
+    lv_obj_set_style_radius(tvoc_arc, 6, 0);
+    lv_obj_set_style_pad_all(tvoc_arc, 4, 0);
 
-    lv_obj_t *tvoc_title = lv_label_create(pressure_panel);
+    lv_obj_t *tvoc_title = lv_label_create(tvoc_arc);
     lv_label_set_text(tvoc_title, "TVOC");
     lv_obj_set_style_text_font(tvoc_title, &lv_font_montserrat_14, 0);
     lv_obj_set_style_text_color(tvoc_title, lv_color_hex(0x333333), 0);
     lv_obj_align(tvoc_title, LV_ALIGN_TOP_LEFT, 4, 2);
 
     // TVOC arc
-    pressure_arc = lv_arc_create(pressure_panel);
-    lv_obj_set_size(pressure_arc, 70, 70);
-    lv_arc_set_rotation(pressure_arc, 135);
-    lv_arc_set_bg_angles(pressure_arc, 0, 270);
-    lv_arc_set_range(pressure_arc, 0, 30);
-    lv_arc_set_value(pressure_arc, 15);
-    lv_obj_remove_style(pressure_arc, NULL, LV_PART_KNOB);
-    lv_obj_set_style_arc_color(pressure_arc, lv_color_hex(0x2196F3), LV_PART_INDICATOR);
-    lv_obj_set_style_arc_color(pressure_arc, lv_color_hex(0xDDDDDD), LV_PART_MAIN);
-    lv_obj_set_style_arc_width(pressure_arc, 8, LV_PART_INDICATOR);
-    lv_obj_set_style_arc_width(pressure_arc, 8, LV_PART_MAIN);
-    lv_obj_align(pressure_arc, LV_ALIGN_CENTER, 10, 8);
+    tvoc_arc_arc = lv_arc_create(tvoc_arc);\
+    lv_obj_set_size(tvoc_arc_arc, 90,90);
+    lv_arc_set_rotation(tvoc_arc_arc, 135);
+    lv_arc_set_bg_angles(tvoc_arc_arc, 0, 270);
+    lv_arc_set_range(tvoc_arc_arc, 0, 30);
+    lv_arc_set_value(tvoc_arc_arc, 15);
+    lv_obj_remove_style(tvoc_arc_arc, NULL, LV_PART_KNOB);
+    lv_obj_set_style_arc_color(tvoc_arc_arc, lv_color_hex(0x2196F3), LV_PART_INDICATOR);
+    lv_obj_set_style_arc_color(tvoc_arc_arc, lv_color_hex(0xDDDDDD), LV_PART_MAIN);
+    lv_obj_set_style_arc_width(tvoc_arc_arc, 8, LV_PART_INDICATOR);
+    lv_obj_set_style_arc_width(tvoc_arc_arc, 8, LV_PART_MAIN);
+    lv_obj_align(tvoc_arc_arc, LV_ALIGN_CENTER, 10, 8);
 
-    pressure_value_label = lv_label_create(pressure_panel);
-    lv_label_set_text(pressure_value_label, "0ppb");
-    lv_obj_set_style_text_font(pressure_value_label, &lv_font_montserrat_14, 0);
-    lv_obj_set_style_text_color(pressure_value_label, lv_color_hex(0x333333), 0);
-    lv_obj_align_to(pressure_value_label, pressure_arc, LV_ALIGN_CENTER, -12, 0);
+    tvoc_arc_value_label = lv_label_create(tvoc_arc);
+    lv_label_set_text(tvoc_arc_value_label, "0ppb");
+    lv_obj_set_style_text_font(tvoc_arc_value_label, &lv_font_montserrat_14, 0);
+    lv_obj_set_style_text_color(tvoc_arc_value_label, lv_color_hex(0x333333), 0);
+    lv_obj_align_to(tvoc_arc_value_label, tvoc_arc_arc, LV_ALIGN_CENTER, -4, 0);
 
     // TVOC scale labels
-    lv_obj_t *tvoc_g_min = lv_label_create(pressure_panel);
+    lv_obj_t *tvoc_g_min = lv_label_create(tvoc_arc);
     lv_label_set_text(tvoc_g_min, "0");
     lv_obj_set_style_text_font(tvoc_g_min, &lv_font_montserrat_14, 0);
     lv_obj_set_style_text_color(tvoc_g_min, lv_color_hex(0x999999), 0);
-    lv_obj_align_to(tvoc_g_min, pressure_arc, LV_ALIGN_OUT_BOTTOM_LEFT, 0, -8);
+    lv_obj_align_to(tvoc_g_min, tvoc_arc_arc, LV_ALIGN_OUT_BOTTOM_LEFT, 0, -8);
 
-    lv_obj_t *tvoc_g_max = lv_label_create(pressure_panel);
+    lv_obj_t *tvoc_g_max = lv_label_create(tvoc_arc);
     lv_label_set_text(tvoc_g_max, "30");
     lv_obj_set_style_text_font(tvoc_g_max, &lv_font_montserrat_14, 0);
     lv_obj_set_style_text_color(tvoc_g_max, lv_color_hex(0x999999), 0);
-    lv_obj_align_to(tvoc_g_max, pressure_arc, LV_ALIGN_OUT_BOTTOM_RIGHT, 0, -8);
+    lv_obj_align_to(tvoc_g_max, tvoc_arc_arc, LV_ALIGN_OUT_BOTTOM_RIGHT, 0, -8);
 }
 
 //  Setup ─
@@ -310,34 +310,34 @@ void loop() {
             float tvoc = sgp.TVOC;
 
             // Update charts
-            lv_chart_set_next_value(chart, temp_series, (int)eco2);
-            lv_chart_set_next_value(pressure_chart, pressure_series, (int)tvoc);
+            lv_chart_set_next_value(chart, eco2_series, (int)eco2);
+            lv_chart_set_next_value(tvoc_arc_chart, tvoc_arc_series, (int)tvoc);
             lv_chart_refresh(chart);
-            lv_chart_refresh(pressure_chart);
+            lv_chart_refresh(tvoc_arc_chart);
 
             // Update arcs
-            lv_arc_set_value(temp_arc, (int)eco2);
-            lv_arc_set_value(pressure_arc, (int)tvoc);
+            lv_arc_set_value(eco2_arc, (int)eco2);
+            lv_arc_set_value(tvoc_arc_arc, (int)tvoc);
 
             // Update value labels
             char buf[16];
             snprintf(buf, sizeof(buf), "%dppm", (int)eco2);
-            lv_label_set_text(temp_value_label, buf);
+            lv_label_set_text(eco2_value_label, buf);
             snprintf(buf, sizeof(buf), "%dppb", (int)tvoc);
-            lv_label_set_text(pressure_value_label, buf);
+            lv_label_set_text(tvoc_arc_value_label, buf);
 
             // eCO2 alert above 415 ppm
             if (eco2 > 415) {
-                lv_obj_set_style_arc_color(temp_arc, lv_color_hex(0xE53935), LV_PART_INDICATOR);
+                lv_obj_set_style_arc_color(eco2_arc, lv_color_hex(0xE53935), LV_PART_INDICATOR);
             } else {
-                lv_obj_set_style_arc_color(temp_arc, lv_color_hex(0x4CAF50), LV_PART_INDICATOR);
+                lv_obj_set_style_arc_color(eco2_arc, lv_color_hex(0x4CAF50), LV_PART_INDICATOR);
             }
 
             // TVOC alert above 25 ppb
             if (tvoc > 25) {
-                lv_obj_set_style_arc_color(pressure_arc, lv_color_hex(0xE53935), LV_PART_INDICATOR);
+                lv_obj_set_style_arc_color(tvoc_arc_arc, lv_color_hex(0xE53935), LV_PART_INDICATOR);
             } else {
-                lv_obj_set_style_arc_color(pressure_arc, lv_color_hex(0x2196F3), LV_PART_INDICATOR);
+                lv_obj_set_style_arc_color(tvoc_arc_arc, lv_color_hex(0x2196F3), LV_PART_INDICATOR);
             }
         }
     }
